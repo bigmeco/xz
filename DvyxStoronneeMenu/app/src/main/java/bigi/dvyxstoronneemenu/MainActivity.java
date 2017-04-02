7e97aba5f1ce0171c3bbe3787f049554540f7016
bigmeco <bigmecoprog@gmail.com>
2017-04-02 20:43:46 +0300

    Komenti

--- DvyxStoronneeMenu/app/src/main/java/bigi/dvyxstoronneemenu/MainActivity.java
+++ DvyxStoronneeMenu/app/src/main/java/bigi/dvyxstoronneemenu/MainActivity.java
@@ -13,7 +13,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        // СОЗДАНИЕ КНОПКИ
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        final DrawerArrowDrawable indicator = new DrawerArrowDrawable(this);
+//        // цвет кнопки меню
+//        indicator.setColor(Color.GREEN);
+//        getSupportActionBar().setHomeAsUpIndicator(indicator);
+
+        // setTransformer();
+
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+//        drawerLayout.setScrimColor(Color.TRANSPARENT);
+//        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+//            @Override
+//            public void onDrawerSlide(View drawerView, float slideOffset) {
+//                if (((ViewGroup) drawerView).getChildAt(1).getId() == R.id.leftSideBar) {
+//                    indicator.setProgress(slideOffset);
+//                }
+//            }
+//        });
+
     }
+
+    // НАСТРОЙКА ОТКЛИКА
+//    private void setTransformer() {
+//        final float spacing = getResources().getDimensionPixelSize(R.dimen.spacing);
+//        SideBar rightSideBar = (SideBar) findViewById(R.id.rightSideBar);
+//        rightSideBar.setTransformer(new Transformer() {
+//            private View lastHoverView;
+//
+//            @Override
+//            public void apply(ViewGroup sideBar, View itemView, float touchY, float slideOffset, boolean isLeft) {
+//                boolean hovered = itemView.isPressed();
+//                if (hovered && lastHoverView != itemView) {
+//                    animateIn(itemView);
+//                    animateOut(lastHoverView);
+//                    lastHoverView = itemView;
+//                }
+//            }
+//
+//            private void animateOut(View view) {
+//                if (view == null) {
+//                    return;
+//                }
+//                ObjectAnimator translationX = ObjectAnimator.ofFloat(view, "translationX", -spacing, 0);
+//                translationX.setDuration(200);
+//                translationX.start();
+//            }
+//
+//            private void animateIn(View view) {
+//                ObjectAnimator translationX = ObjectAnimator.ofFloat(view, "translationX", 0, -spacing);
+//                translationX.setDuration(200);
+//                translationX.start();
+//            }
+//        });
+//    }
+
+
+    // ПО НАЖАТИЮ КНОПКИ МЕНЮ
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == android.R.id.home) {
+//            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//                drawerLayout.closeDrawer(GravityCompat.START);
+//            } else {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        }
+//        return true;
+//    }
+// КОН
     public void onClick(View view) {
         if (view instanceof TextView) {
             String title = ((TextView) view).getText().toString();
