d951c9e318773831cfda759fe8b2cc1d2d4ed0fe
bigmeco <bigmecoprog@gmail.com>
2017-04-03 17:52:31 +0300

    ImgUgol

--- DvyxStoronneeMenu/app/src/main/java/bigi/dvyxstoronneemenu/MainActivity.java
+++ DvyxStoronneeMenu/app/src/main/java/bigi/dvyxstoronneemenu/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
         // СОЗДАНИЕ КНОПКИ
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        final DrawerArrowDrawable indicator = new DrawerArrowDrawable(this);
